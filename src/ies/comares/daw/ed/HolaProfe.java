c7b0c302ad4f73917bef0ed04c971fbce89e65cd
Ruben Arjona <ruben@gmail.com>
2024-10-22 12:56:40 +0200

    jeje

--- src/ies/comares/daw/ed/HolaProfe.java
+++ src/ies/comares/daw/ed/HolaProfe.java
@@ -6,6 +6,7 @@ public class HolaProfe {
 		System.out.println("Hola mi nombre es : Rafael Nieto Campos");
 		System.out.println("Hi my name is: Ruben Arjona Lanza");
 		System.out.println("Hi Ruben how are you?");
+		System.out.println("Im fine thanks jeje this is really ");
 	}
 
 }
