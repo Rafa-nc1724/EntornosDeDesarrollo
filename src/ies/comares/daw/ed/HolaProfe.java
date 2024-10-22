a63af0310791ed2027086a4eaff3de6f7fffad98
Rafa Nieto <rafanietocampos@gmail.com>
2024-10-22 12:54:18 +0200

    hi

--- src/ies/comares/daw/ed/HolaProfe.java
+++ src/ies/comares/daw/ed/HolaProfe.java
@@ -5,6 +5,7 @@ public class HolaProfe {
 	public static void main(String[] args) {
 		System.out.println("Hola mi nombre es : Rafael Nieto Campos");
 		System.out.println("Hi my name is: Ruben Arjona Lanza");
+		System.out.println("Hi Ruben how are you?");
 	}
 
 }
